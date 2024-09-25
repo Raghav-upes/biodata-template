631daa3f8014715ec9bb04896a3fd4ff20324982
Harsh Mangal <116105912+harsh-mangal@users.noreply.github.com>
2024-09-25 13:51:56 +0530

    fixed

--- client/src/Components/Document.jsx
+++ client/src/Components/Document.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react';
-import Man from '../Images/Man.png';
+import React, { useEffect, useRef, useState } from "react";
+import Man from "../Images/Man.png";
 
 export default function Document() {
   const componentRef = useRef(null);
@@ -8,7 +8,7 @@ export default function Document() {
 
   useEffect(() => {
     // Retrieve form data from localStorage
-    const savedFormData = JSON.parse(localStorage.getItem('formData'));
+    const savedFormData = JSON.parse(localStorage.getItem("formData"));
     if (savedFormData) {
       setFormData(savedFormData);
     }
@@ -26,11 +26,11 @@ export default function Document() {
       }
     }
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     handleResize(); // Call once to set initial size
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
@@ -41,87 +41,154 @@ export default function Document() {
   const { personalDetails, familyDetails, contactDetails } = formData;
 
   return (
-    <div className="centered-component" ref={componentRef} style={{ transform: `scale(${scale})` }}>
+    <div
+      className="centered-component"
+      ref={componentRef}
+      style={{ transform: `scale(${scale})` }}
+    >
       <div className="biodata">
         <section>
           <table>
             <tbody>
               <tr>
-                <td><h4 style={{ marginTop: "0px" }}><b>Personal Details</b></h4></td>
+                <td>
+                  <h4 style={{ marginTop: "0px" }}>
+                    <b>Personal Details</b>
+                  </h4>
+                </td>
               </tr>
               <tr>
                 <td>Name</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{personalDetails.name}</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{personalDetails.name}</p>
+                </td>
               </tr>
               <tr>
                 <td>Date Of Birth</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{personalDetails.dob}</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{personalDetails.dob}</p>
+                </td>
               </tr>
               <tr>
                 <td>Place Of Birth</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{personalDetails.placeOfBirth}</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{personalDetails.placeOfBirth}</p>
+                </td>
               </tr>
               <tr>
                 <td>Time Of Birth</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{personalDetails.timeOfBirth}</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{personalDetails.timeOfBirth}</p>
+                </td>
               </tr>
               <tr>
                 <td>Rashi</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{personalDetails.rashi}</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{personalDetails.rashi}</p>
+                </td>
               </tr>
               <tr>
                 <td>Nakshatra</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{personalDetails.nakshatra}</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{personalDetails.nakshatra}</p>
+                </td>
               </tr>
               <tr>
                 <td>Complexion</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{personalDetails.complexion}</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{personalDetails.complexion}</p>
+                </td>
               </tr>
               <tr>
                 <td>Height</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{personalDetails.height}</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{personalDetails.height}</p>
+                </td>
               </tr>
               <tr>
                 <td>Gotra</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{personalDetails.gotra}</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{personalDetails.gotra}</p>
+                </td>
               </tr>
               <tr>
                 <td>Bachelors</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{personalDetails.bachelors}</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{personalDetails.bachelors}</p>
+                </td>
               </tr>
               <tr>
                 <td>Work</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{personalDetails.work}</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{personalDetails.work}</p>
+                </td>
               </tr>
               <tr>
-                <td><h4><b>Family Details</b></h4></td>
+                <td>
+                  <h4>
+                    <b>Family Details</b>
+                  </h4>
+                </td>
               </tr>
               <tr>
                 <td>Father's Name</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{familyDetails.fatherName}</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{familyDetails.fatherName}</p>
+                </td>
               </tr>
               <tr>
                 <td>Mother's Name</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{familyDetails.motherName}</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{familyDetails.motherName}</p>
+                </td>
               </tr>
               <tr>
-                <td><h4><b>Contact Details</b></h4></td>
+                <td>
+                  <h4>
+                    <b>Contact Details</b>
+                  </h4>
+                </td>
               </tr>
               <tr>
                 <td>Contact Person</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{contactDetails.contactPerson}</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{contactDetails.contactPerson}</p>
+                </td>
               </tr>
               <tr>
                 <td>Contact Number</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{contactDetails.contactNumber}</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{contactDetails.contactNumber}</p>
+                </td>
               </tr>
               <tr>
                 <td>Email ID</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{contactDetails.email}</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{contactDetails.email}</p>
+                </td>
               </tr>
               <tr>
                 <td>Residential Address</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{contactDetails.address}</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{contactDetails.address}</p>
+                </td>
               </tr>
             </tbody>
           </table>
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Hero from '../Components/Hero'
 import Form from '../Components/Form'
 import Template from './Template'
-
+import TemplateCreameAndWhite from '../Components/TemplateCreameAndWhite'
+import TemplateDarkBlue from '../Components/TemplateDarkBlue'
 const Home = () => {
   return (
     <div>
         <Hero/>
         <Form/>
         <Template/>
+        <TemplateCreameAndWhite/>
+        <TemplateDarkBlue/>
     </div>
   )
 }
