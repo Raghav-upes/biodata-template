97222b9b8cba542b9f88b95478b73fc7db97c6b2
Harsh Mangal <116105912+harsh-mangal@users.noreply.github.com>
2024-09-25 14:12:00 +0530

    fetched from local

--- client/src/Components/TemplateCreameAndWhite.jsx
+++ client/src/Components/TemplateCreameAndWhite.jsx
@@ -1,11 +1,17 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Man from '../Images/Man.png';
 import Frame from '../Images/Group 1.png';
+
 export default function TemplateCreameAndWhite() {
   const componentRef = useRef(null);
   const [scale, setScale] = useState(1);
+  const [userData, setUserData] = useState({});
 
   useEffect(() => {
+    // Fetch data from localStorage when component mounts
+    const savedData = JSON.parse(localStorage.getItem('formData')) || {};
+    setUserData(savedData);
+
     function handleResize() {
       if (componentRef.current) {
         const availableWidth = window.innerWidth * 0.7; // 70% width for the main content area
@@ -29,116 +35,182 @@ export default function TemplateCreameAndWhite() {
 
   return (
     <div className="centered-component-creame-white" ref={componentRef} style={{ transform: `scale(${scale})` }}>
-            <img src={Frame} className='frame' />
+      <img src={Frame} className="frame" alt="Frame" />
       <div className="biodata">
-
         <section>
           <table>
             <tbody>
               <tr>
-                <td className='Heading'><div className='Small-Box'></div><h4 style={{ marginTop: "0px" }}><b>Personal Details</b></h4></td>
+                <td className="Heading">
+                  <div className="Small-Box"></div>
+                  <h4 style={{ marginTop: "0px" }}>
+                    <b>Personal Details</b>
+                  </h4>
+                </td>
               </tr>
-          <tr>
-            <td colSpan={2}><hr /></td>
-          </tr>
               <tr>
-                <td className='bold'>Name</td>
-                <td className='value-data bold'><p>:&emsp;&nbsp;</p><p>Raghav Verma Sharma</p></td>
+                <td colSpan={2}>
+                  <hr />
+                </td>
+              </tr>
+              <tr>
+                <td className="bold">Name</td>
+                <td className="value-data bold">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{userData?.personalDetails?.name || 'N/A'}</p>
+                </td>
               </tr>
               <tr>
                 <td>Date Of Birth</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>02/07/2024</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{userData?.personalDetails?.dob ? new Date(userData.personalDetails.dob).toLocaleDateString() : 'N/A'}</p>
+                </td>
               </tr>
               <tr>
                 <td>Place Of Birth</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>bhhghj</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{userData?.personalDetails?.placeOfBirth || 'N/A'}</p>
+                </td>
               </tr>
               <tr>
                 <td>Time Of Birth</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>02:00 AM</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{userData?.personalDetails?.timeOfBirth ? new Date(userData.personalDetails.timeOfBirth).toLocaleTimeString() : 'N/A'}</p>
+                </td>
               </tr>
               <tr>
                 <td>Rashi</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>Mesh (Aries)</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{userData?.personalDetails?.rashi || 'N/A'}</p>
+                </td>
               </tr>
               <tr>
                 <td>Nakshatra</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>gfgf</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{userData?.personalDetails?.nakshatra || 'N/A'}</p>
+                </td>
               </tr>
               <tr>
                 <td>Complexion</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>Very Fair</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{userData?.personalDetails?.complexion || 'N/A'}</p>
+                </td>
               </tr>
               <tr>
                 <td>Height</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>6 feet 4 inches</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{userData?.personalDetails?.height || 'N/A'}</p>
+                </td>
               </tr>
               <tr>
                 <td>Gotra</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>fdggfgfd</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{userData?.personalDetails?.gotra || 'N/A'}</p>
+                </td>
               </tr>
               <tr>
                 <td>Bachelors</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>fdggfd</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{userData?.personalDetails?.bachelors || 'N/A'}</p>
+                </td>
               </tr>
               <tr>
                 <td>Work</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>fdgfgd</p></td>
-              </tr>
-              <tr >
-                <td className='Heading' style={{ marginTop: "20px" }}><div className='Small-Box'></div><h4 style={{ marginTop: "0px" }}><b>Family Details</b></h4></td>
-              </tr>
-              <tr>
-            <td colSpan={2}><hr /></td>
-          </tr>
-              <tr>
-                <td >Father's Name</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>fdggfd</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{userData?.personalDetails?.work || 'N/A'}</p>
+                </td>
               </tr>
               <tr>
-                <td>Father's Occupation</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>dgfdgfd</p></td>
+                <td className="Heading" style={{ marginTop: "20px" }}>
+                  <div className="Small-Box"></div>
+                  <h4 style={{ marginTop: "0px" }}>
+                    <b>Family Details</b>
+                  </h4>
+                </td>
+              </tr>
+              <tr>
+                <td colSpan={2}>
+                  <hr />
+                </td>
+              </tr>
+              <tr>
+                <td>Father's Name</td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{userData?.familyDetails?.fatherName || 'N/A'}</p>
+                </td>
               </tr>
               <tr>
                 <td>Mother's Name</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>fdfgdgfdg</p></td>
-              </tr>
-              <tr>
-                <td>Mother's Occupation</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>dgfgfd</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{userData?.familyDetails?.motherName || 'N/A'}</p>
+                </td>
               </tr>
               <tr>
                 <td>Siblings</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>fggfd</p></td>
-              </tr>
-              <tr >
-                <td className='Heading' style={{ marginTop: "20px" }}><div className='Small-Box'></div><h4 style={{ marginTop: "0px" }}><b>Contact Details</b></h4></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{userData?.familyDetails?.siblings || 'N/A'}</p>
+                </td>
               </tr>
               <tr>
-            <td colSpan={2}><hr /></td>
-          </tr>
+                <td className="Heading" style={{ marginTop: "20px" }}>
+                  <div className="Small-Box"></div>
+                  <h4 style={{ marginTop: "0px" }}>
+                    <b>Contact Details</b>
+                  </h4>
+                </td>
+              </tr>
+              <tr>
+                <td colSpan={2}>
+                  <hr />
+                </td>
+              </tr>
               <tr>
                 <td>Contact Person</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>dgfgfd</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{userData?.contactDetails?.contactPerson || 'N/A'}</p>
+                </td>
               </tr>
               <tr>
                 <td>Contact Number</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>dgfgfd</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{userData?.contactDetails?.contactNumber || 'N/A'}</p>
+                </td>
               </tr>
               <tr>
                 <td>Email ID</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>gddgf</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{userData?.contactDetails?.email || 'N/A'}</p>
+                </td>
               </tr>
               <tr>
                 <td>Residential Address</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>fdggfdgfd</p></td>
+                <td className="value-data">
+                  <p>:&emsp;&nbsp;</p>
+                  <p>{userData?.contactDetails?.address || 'N/A'}</p>
+                </td>
               </tr>
             </tbody>
           </table>
         </section>
       </div>
       {/* <div className="img-container">
-        <img src={Man} alt="" />
+        <img src={Man} alt="Man" />
       </div> */}
     </div>
   );
--- client/src/Components/TemplateDarkBlue.jsx
+++ client/src/Components/TemplateDarkBlue.jsx
@@ -2,11 +2,20 @@ import React, { useEffect, useRef, useState } from 'react';
 import Man from '../Images/Man.png';
 import LineBreak from '../Images/CutBreakLine.png';
 import picBorder from '../Images/—Pngtree—circle gold frame_6932380.png';
-import Rangoli from "../Images/Rangoli2.png"
+import Rangoli from "../Images/Rangoli2.png";
 
 export default function TemplateDarkBlue() {
   const componentRef = useRef(null);
   const [scale, setScale] = useState(1);
+  const [biodata, setBiodata] = useState({});
+
+  useEffect(() => {
+    // Fetch data from local storage
+    const data = JSON.parse(localStorage.getItem('formData')); // Assuming 'userData' is the key
+    if (data) {
+      setBiodata(data);
+    }
+  }, []);
 
   useEffect(() => {
     function handleResize() {
@@ -32,71 +41,59 @@ export default function TemplateDarkBlue() {
 
   return (
     <div className="centered-component-dark-blue" ref={componentRef} style={{ transform: `scale(${scale})` }}>
-              <img className='Rangoli' src={Rangoli} alt="" />
-              <img className='Rangoli2' src={Rangoli} alt="" />
-              <img className='Rangoli3' src={Rangoli} alt="" />
-              <img className='Rangoli4' src={Rangoli} alt="" />
+      <img className='Rangoli' src={Rangoli} alt="" />
+      <img className='Rangoli2' src={Rangoli} alt="" />
+      <img className='Rangoli3' src={Rangoli} alt="" />
+      <img className='Rangoli4' src={Rangoli} alt="" />
       <div className="biodata">
 
         <div className="">
-      <img style={{zIndex:1,position:"absolute",width:'140px',height:'130px',paddingRight:'10px',paddingBottom:"10px",left:"50%",transform:"translateX(-50%)"}} src={picBorder} alt="" />
-      <div className="img-container">
-       
-        <img src={Man} alt="" />
-      </div>
-      </div>
+          <img style={{ zIndex: 1, position: "absolute", width: '140px', height: '130px', paddingRight: '10px', paddingBottom: "10px", left: "50%", transform: "translateX(-50%)" }} src={picBorder} alt="" />
+          <div className="img-container">
+            <img src={Man} alt="" />
+          </div>
+        </div>
         <section>
           <table>
             <tbody>
-              {/* <tr>
-                <td><h4 style={{ marginTop: "0px" }}><b>Personal Details</b></h4></td>
-              </tr>
               <tr>
-                <td>Name</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>Raghav Verma Sharma</p></td>
-              </tr> */}
-              <tr>
-       <td colSpan={2}><h2>Raghav Verma Sharma</h2></td>
+                <td colSpan={2}><h2>{biodata?.personalDetails?.name || 'Name'}</h2></td>
               </tr>
               <tr>
                 <td>Date Of Birth</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>02/07/2024</p></td>
+                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{biodata?.personalDetails?.dob ? new Date(biodata.personalDetails.dob).toLocaleDateString() : 'DOB'}</p></td>
               </tr>
               <tr>
                 <td>Place Of Birth</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>bhhghj</p></td>
+                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{biodata?.placeOfBirth || 'Place'}</p></td>
               </tr>
               <tr>
                 <td>Time Of Birth</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>02:00 AM</p></td>
+                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{biodata?.personalDetails?.timeOfBirth ? new Date(biodata.personalDetails.timeOfBirth).toLocaleTimeString() : 'Time'}</p></td>
               </tr>
               <tr>
                 <td>Rashi</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>Mesh (Aries)</p></td>
+                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{biodata?.rashi || 'Rashi'}</p></td>
               </tr>
               <tr>
                 <td>Nakshatra</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>gfgf</p></td>
+                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{biodata?.nakshatra || 'Nakshatra'}</p></td>
               </tr>
               <tr>
                 <td>Complexion</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>Very Fair</p></td>
+                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{biodata?.complexion || 'Complexion'}</p></td>
               </tr>
               <tr>
                 <td>Height</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>6 feet 4 inches</p></td>
+                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{biodata?.height || 'Height'}</p></td>
               </tr>
               <tr>
                 <td>Gotra</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>fdggfgfd</p></td>
+                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{biodata?.gotra || 'Gotra'}</p></td>
               </tr>
-              {/* <tr>
-                <td>Bachelors</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>fdggfd</p></td>
-              </tr> */}
               <tr>
                 <td>Work</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>fdgfgd</p></td>
+                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{biodata?.work || 'Work'}</p></td>
               </tr>
               <tr>
                 <td colSpan={2}><img src={LineBreak} className='line-break' alt="" /></td>
@@ -106,23 +103,11 @@ export default function TemplateDarkBlue() {
               </tr>
               <tr>
                 <td>Father's Name</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>fdggfd</p></td>
-              </tr>
-              <tr>
-                <td>Father's Occupation</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>dgfdgfd</p></td>
+                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{biodata?.familyDetails?.fatherName || 'Father\'s Name'}</p></td>
               </tr>
               <tr>
                 <td>Mother's Name</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>fdfgdgfdg</p></td>
-              </tr>
-              <tr>
-                <td>Mother's Occupation</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>dgfgfd</p></td>
-              </tr>
-              <tr>
-                <td>Siblings</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>fggfd</p></td>
+                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{biodata?.familyDetails?.motherName || 'Mother\'s Name'}</p></td>
               </tr>
               <tr>
                 <td colSpan={2}><img src={LineBreak} className='line-break' alt="" /></td>
@@ -132,25 +117,24 @@ export default function TemplateDarkBlue() {
               </tr>
               <tr>
                 <td>Contact Person</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>dgfgfd</p></td>
+                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{biodata?.contactDetails?.contactPerson || 'Contact Person'}</p></td>
               </tr>
               <tr>
                 <td>Contact Number</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>dgfgfd</p></td>
+                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{biodata?.contactDetails?.contactNumber || 'Contact Number'}</p></td>
               </tr>
               <tr>
                 <td>Email ID</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>gddgf</p></td>
+                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{biodata?.contactDetails?.email || 'Email'}</p></td>
               </tr>
               <tr>
                 <td>Residential Address</td>
-                <td className='value-data'><p>:&emsp;&nbsp;</p><p>fdggfdgfd</p></td>
+                <td className='value-data'><p>:&emsp;&nbsp;</p><p>{biodata?.address || 'Address'}</p></td>
               </tr>
             </tbody>
           </table>
         </section>
       </div>
-
     </div>
   );
 }
